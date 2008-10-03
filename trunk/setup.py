93e96050719adbe2bd0cd3720540df17dc9a3939
paul.joseph.davis <paul.joseph.davis@4f6427df-9a4a-0410-8c60-f9fd434fc42b>
2008-10-03 19:58:27 +0000

    Bumped the version number before updating pypi.

--- trunk/setup.py
+++ trunk/setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='nosexml',
-    version='0.1',
+    version='0.2',
     author='Paul Davis',
     author_email = 'davisp@neb.com',
     description = 'Format nosetests output in xml',
